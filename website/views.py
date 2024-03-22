9150df799f6e2ae69caae635c147309180125e8c
unknown <vishnuvpk05@gmail.com>
2024-03-22 22:58:07 +0530

    feat: Projects CRUD API and UI

--- website/views.py
+++ website/views.py
@@ -29,3 +29,66 @@ def newfundme():
 
         return redirect(url_for("views.projects"))
     return render_template("newfundme.html", user=current_user)
+
+@views.route("/projects", methods=["GET", "POST"])
+def projects():
+    connection = connect_to_database()
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM crowdfund"
+
+    cursor.execute(query)
+
+    projects = cursor.fetchall()
+    connection.close()
+    return render_template("projects.html", user=current_user, projects=projects)
+
+@views.route("myprojects", methods=["GET", "POST"])
+def myprojects():
+    connection = connect_to_database()
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM crowdfund WHERE username = %s"
+    cursor.execute(query, (current_user.id,))
+
+    projects = cursor.fetchall()
+    connection.close()
+    return render_template("myprojects.html", user=current_user, projects=projects)
+
+@views.route("/editfundme/<string:id>", methods=["GET", "POST"])
+def editfundme(id):
+    connection = connect_to_database()
+    cursor = connection.cursor()
+
+    if request.method == "POST":
+        new_title = request.form.get("title")
+        new_description = request.form.get("description")
+        new_image = request.form.get("image")
+        new_goal = request.form.get("total_funding")
+
+        query = "UPDATE crowdfund SET title = %s, description = %s, image = %s, goal = %s WHERE title = %s"
+        cursor.execute(query, (new_title, new_description, new_image, new_goal, id))
+        connection.commit()
+
+        connection.close()
+        flash("Project updated successfully!", "success")
+        return redirect(url_for("views.myprojects"))
+
+    query = "SELECT * FROM crowdfund WHERE title = %s"
+    cursor.execute(query, (id,))
+    project = cursor.fetchone()
+    connection.close()
+
+    return render_template("editfundme.html", user=current_user, project=project)
+
+@views.route("/deletefundme/<string:id>", methods=["GET", "POST"])
+def deletefundme(id):
+    connection = connect_to_database()
+    cursor = connection.cursor()
+
+    query = "DELETE FROM crowdfund WHERE title = %s"
+    cursor.execute(query, (id,))
+    connection.commit()
+    connection.close()
+    flash("Project deleted successfully!", "success")
+    return redirect(url_for("views.myprojects"))
